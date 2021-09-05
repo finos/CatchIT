d899ba857a1006d88d1449d9c2dbe2137c5f295e
Aniruddha <aniruddha97bhatt@gmail.com>
2021-09-05 20:08:37 +0530

    Add initial tests for the catchit suite

--- tests/test_catchit.py
+++ tests/test_catchit.py
@@ -2,9 +2,9 @@ import json
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent.parent / "catchit")) # noqa
+sys.path.append(str(Path(__file__).parent.parent / "catchit"))  # noqa
 
-import catchit
+import catchit # type: ignore
 
 with open("catchit/regexs.json", "r") as regexs:
     REGEXS_DICT = json.loads(regexs.read())
@@ -22,8 +22,36 @@ def test_find(tmp_path):
     rsa_file.touch()
     rsa_file.write_text("Dummy Value")
 
-    catchit_results = catchit.exec_find(REGEXS_DICT, str(Path(dir).resolve()))
+    catchit_results = catchit.exec_find(REGEXS_DICT, str(dir))
     assert sorted([finding["file_key"] for finding in catchit_results]) == [
         "KEY",
         "RSA_KEYS",
+    ]
+
+    sub_dir = dir / "sub_dir"
+    assert catchit.exec_find(REGEXS_DICT, str(sub_dir)) == []
+
+
+def test_grep(tmp_path):
+    dir = tmp_path / "sub"
+    dir.mkdir()
+
+    txt_file = dir / "sample.txt"
+    txt_file.touch()
+    txt_file.write_text(
+        """
+password = "fsdfdsfdsfdfgdfg1234"
+
+-u anirudd -p asfddsfdfdfgfdg
+
+this_is_not_a_key = AKIAAIOSFODNN7EXAMPLE
+    
+    """
+    )
+
+    catchit_results = catchit.exec_grep(REGEXS_DICT, dir)
+    assert sorted([finding["regex_key"] for finding in catchit_results]) == [
+        "AWS-ID",
+        "PASSWORD",
+        "PASSWORD-ARGUMENT",
     ]
