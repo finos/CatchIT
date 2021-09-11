51c857bb8dc7cdbbc45a9fee3ad11d33f817279c
Aniruddha <aniruddha97bhatt@gmail.com>
2021-09-11 14:41:11 +0530

    Bumping python version to 3.9 for black lint test

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 poetry run flake8 catchit/ tests/
 poetry run isort --profile black --check --diff catchit/ tests/
-poetry run black --target-version py36 --check catchit/ tests/
+poetry run black --target-version py39 --check catchit/ tests/
